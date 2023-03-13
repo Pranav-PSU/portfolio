43a7dd72b0660c351f1ab7e7a5febafa1aacfc59
Pranav-PSU <pran4@pdx.edu>
2023-03-13 16:10:30 -0700

    #Files for CSS are creted #CSS for HTML code added #JQuery code for modal added

---
+++ index.js
@@ -0,0 +1,64 @@
+    let pinkyPromiseHtml = `<ul>
+    <li> Developed full-Stack projects, REST APIs, back-end business logic, complex algorithms, and scheduling functions that run on specific time intervals.
+  </li>
+    <li>Implemented modules like Chat-bot, Chatroom, and E-commerce along with communication APIs which are Twilio, and Nexmo. </li>
+    <li>Learned data transmission and network protocols, designing high-quality software and architecture.</li>
+  </ul>  
+  `;
+  let pinkyPromiseTitle= 'Senior Software Developer (Contract)';
+
+  let asplHtml = `<ul>
+  <li> Developed highly functional full-Stack projects in MEAN, REST APIs, complex back-end logic algorithms, API authentication, and security.</li>
+  <li> Involved in designing and developing of hybrid mobile applications in Flutter and Ionic Framework.</li>
+  <li>Handled Team-Lead position successfully and ensured all projects are completed in time and to company specifications.</li>
+  <li>Learned and Involved in task prioritizing, planning and estimation, and creating user flows and prototypes.</li>
+  </ul>  
+  `;
+  let asplTitle = 'Senior Software Developer'
+
+  let kaldinHtml = `<ul>
+  <li> Developed customized and highly functional CRM systems in JAVA along with the integration of third-party APIs which are SMS and Email Gateways, Google Maps, Directions API, etc.</li>
+  <li>Developed new functionalities, and technologies and integrate them into existing functions.</li>
+  <li>Involved in software development, assisted the development team and manager with all aspects of software designing and coding, support, analysis, and all related activities.</li>
+</ul>  
+`;
+let kaldinTitle= 'Junior Software Associate'
+    
+    $('.cardClickOpenModal').click(function(){
+   let html = '';
+   let title = '';
+      switch(this.id)
+        {
+          case "pinkyPromiseCardModalToggle":{
+            html = pinkyPromiseHtml;
+            title = pinkyPromiseTitle;
+            break;
+          }
+          case "kaldinCardModalToggle":{
+            html = kaldinHtml;
+            title = kaldinTitle;
+            break;
+          }
+          case "asplCardModalToggle":{
+            html=asplHtml;
+            title = asplTitle;
+            break;
+          }
+          default:{
+            console.warn("Invalid Data");
+          }
+        }
+        $('.modal-body').empty();
+        $('.modal-title').empty();
+        $('<h5>').html(title).appendTo('.modal-title');
+        // Append new content to the modal body
+        $('<p>').html(html).appendTo('.modal-body');
+        
+        // Show the modal
+        $('#ProjectDescriptionModal').modal('show');
+    });
+    
+
+   
+
+      
