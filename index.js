f2353074f1f2580f412eb9fde7399e605b2b9c8c
Pranav-PSU <pran4@pdx.edu>
2023-08-09 19:27:31 -0700

    More information added

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 // Job roles and titles for experience
 let pinkyPromiseHtml = `<ul>
-    <li> Developed full-Stack projects, REST APIs, back-end business logic, complex algorithms, and scheduling functions that run on specific time intervals.
-  </li>
-    <li>Implemented modules like Chat-bot, Chatroom, and E-commerce along with communication APIs which are Twilio, and Nexmo. </li>
-    <li>Learned data transmission and network protocols, designing high-quality software and architecture.</li>
+<li>Architected and developed full-stack solutions, REST APIs, and integrated scheduling functions adhering to best-in-class standards. Incorporated robust security measures using encryption, SSL, and HTTPS to ensure data protection</li>
+<li>Spearheaded the design and implementation of features such as Chat-bot, Chatroom, and E-commerce, seamlessly integrating communication APIs like Twilio and Nexmo</li>
+<li>Deepened proficiency in rigorous unit testing and ensured consistent code quality through automated testing frameworks like Jest, Jasmine, Mocha etc</li>
+<li>Collaborated in agile teams, actively participating in sprint planning, retrospectives, and continuous integration processes</li>
   </ul>  
   `;
 let pinkyPromiseTitle = "Senior Software Developer (Contract)";
 
 let asplHtml = `<ul>
-  <li> Developed highly functional full-Stack projects in MEAN, REST APIs, complex back-end logic algorithms, API authentication, and security.</li>
-  <li> Involved in designing and developing of hybrid mobile applications in Flutter and Ionic Framework.</li>
-  <li>Handled Team-Lead position successfully and ensured all projects are completed in time and to company specifications.</li>
-  <li>Learned and Involved in task prioritizing, planning and estimation, and creating user flows and prototypes.</li>
+<li>Architected and developed robust full-stack projects using the MEAN stack, enhancing API security and performance</li>
+<li>Led the design and deployment of hybrid mobile applications using of Flutter and React Native</li>
+<li>Successfully managed a team of 14 developers, guaranteeing timely project delivery meeting company standards</li>
+<li>Deepened proficiency in unit testing and testing strategies, assuring application stability and reliability</li>
+<li>Embraced agile methodologies, facilitating efficient backlog grooming, sprint planning, and iterative development</li>
   </ul>  
   `;
 let asplTitle = "Senior Software Developer";
 
 let kaldinHtml = `<ul>
-  <li> Developed customized and highly functional CRM systems in JAVA along with the integration of third-party APIs which are SMS and Email Gateways, Google Maps, Directions API, etc.</li>
-  <li>Developed new functionalities, and technologies and integrate them into existing functions.</li>
-  <li>Involved in software development, assisted the development team and manager with all aspects of software designing and coding, support, analysis, and all related activities.</li>
+<li>Custom-built CRM system, Vehicle tracking system using Java, integrating third-party APIs such as SMS and Email Gateways, Google Maps, and Directions API, ensuring optimized user experience and extended functionality</li>
+<li>Contributed holistically to the software development cycle, supporting the team and management in design, coding, analysis, and related tasks</li>
+<li>Adopted unit testing best practices and deployed testing frameworks, ensuring software reliability and quality</li>
 </ul>  
 `;
 let kaldinTitle = "Junior Software Associate";
